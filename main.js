c9c4511e2d8648899fded1c8ed76c3bf9bc29d1d
Joshua <yoonj200@gmail.com>
2022-02-15 19:55:59 -0600

    commit tree 636f9f0f8773343d61e8eaf1b91775c640928663

--- main.js
+++ main.js
@@ -3,9 +3,48 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.querySelector("#modal");
 
+hideError();
 
+document.addEventListener("DOMContentLoaded", () => {
+  //const errorModal = document.querySelector("#modal"); --------------- already declared line 6 
+  console.log("DOM Content has loaded!");
+  //errorModal.classList.toggle('hidden'); -------------- already called hideError() line 8
 
+  clickListener();
+});
+
+function hideError() {
+  errorModal.classList.toggle('hidden');
+}
+
+function clickListener() {
+  document.addEventListener('click', (event) => {
+    if (event.target.classList[0] === 'like-glyph') {
+      //PROMISE, async we need a .then
+      
+      mimicServerCall()
+        .then(resp => {
+          const activated = event.target.classList.contains("activated-heart");
+          if(activated) {
+            event.target.classList.remove("activated-heart")
+            event.target.innerHTML = EMPTY_HEART
+          } else {
+            event.target.classList.add("activated-heart")
+            event.target.innerHTML = FULL_HEART
+            //use toggle() instead of add()
+            //add() might add the class multiple times
+          }
+        })
+        .catch(error => { 
+          setTimeout(() => {
+            hideError()
+          }, 3000)
+          }) // PROMISE FAILS, .catch -> catches it
+      }
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -14,7 +53,7 @@ const FULL_HEART = '♥'
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+      let isRandomFailure = Math.random() < 0.2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,6 @@
 const sinon = require( 'sinon' );
 const helpers = require( './helpers' );
-const chai = require( 'chai' );
+const chai = require( 'chai' ); // test library
 const spies = require( 'chai-spies' );
 
 chai.use( spies );
@@ -8,6 +8,7 @@ chai.use( spies );
 describe( "main.js", () => {
   it("contains a hidden modal", () => {
     let modal = document.querySelector('.hidden')
+    //console.log(modal.classList) -> helps with debugging :)
     expect(modal).not.to.equal(null)
   } )
 } )
